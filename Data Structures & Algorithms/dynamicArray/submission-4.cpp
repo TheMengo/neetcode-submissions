9ef5bc4ab78fc61e1b597c148160ed8e50cd1c16
Meng Shi <113142761+TheMengo@users.noreply.github.com>
2026-06-05 19:46:39 -0400

    Add: dynamicArray - submission-4

---
+++ Data Structures & Algorithms/dynamicArray/submission-4.cpp
@@ -0,0 +1,55 @@
+class DynamicArray {
+private:
+    int* arr;
+    int size;
+    int capacity;
+public:
+
+    DynamicArray(int capacity) : capacity(capacity) {
+        arr = new int[capacity];
+        size = 0;
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if(size == capacity){
+            resize();
+        }
+        arr[size] = n;
+        size++;
+
+    }
+
+    int popback() {
+        if(size > 0){
+            size--;
+        }
+        return arr[size];
+    }
+
+    void resize() {
+        capacity *= 2;
+        int* newArr = new int[capacity];
+        for(int i = 0; i < size; i++){
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
+
+    int getSize() {
+        return size;
+
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+};
