d44741c63a87054befef721bb239ab94d5cda90d
Meng Shi <113142761+TheMengo@users.noreply.github.com>
2026-07-08 09:19:04 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int res = 0;
+        int maxl = height[l], maxr = height[r];
+        while(r > l){
+            if(maxl < maxr){
+                l++;
+                maxl = max(maxl, height[l]);
+                res += maxl - height[l];
+            }else{
+                r--;
+                maxr = max(maxr, height[r]);
+                res += maxr - height[r];
+            }
+        }
+        return res;
+    }
+};
